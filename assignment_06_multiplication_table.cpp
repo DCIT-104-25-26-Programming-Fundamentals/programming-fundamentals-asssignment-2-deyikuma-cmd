55bec74035e2fe24224d5f4a38405f4100fedefd
Deyikuma <deyikuma@gmail.com>
2026-08-01 16:52:09 +0000

    Add multiplication table generator

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,40 @@
 #include <iostream>
 using namespace std;
 
+void printTable(int number) {
+    cout << "Multiplication Table for " << number << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << number << " x " << i << " = " << (number * i) << endl;
+    }
+}
+
+void printTablesUpTo(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        printTable(num);
+        if (num != n) {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main() {
+    // ----- PART A -----
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    printTable(number);
+
+    // ----- PART B -----
+    cout << endl;
+    int n;
+    cout << "Enter N (tables from 1 to N): ";
+    cin >> n;
+    printTablesUpTo(n);
+
+    return 0;
+}
